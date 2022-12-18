6395765dcbf1daa1985abb4d7856ef1d511c3db0
c451 <codeallright@protonmail.com>
2022-12-18 15:40:06 +0400

    Add Cloud overlay

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 import viteRawPlugin from "./vite/vite-raw-plugin.js";
+import kill from 'kill-port'
+
+
+kill(16888, 'tcp').then(() => {}).catch(() => {})
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +20,7 @@ export default defineConfig({
   ],
   server: {
     port: 16888,
+    strictPort: true
   },
   build: {
     target: "es2018"
